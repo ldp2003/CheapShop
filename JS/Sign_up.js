ee510b9fcd3c267aaae43df53db2314a63e93410
Pkkoi1 <khoinhokboddy@gmail.com>
2023-04-21 14:16:29 +0700

    Thêm regex cho đăng nhập và đăng ký

---
+++ JS/Sign_up.js
@@ -0,0 +1,41 @@
+function ktratendn() {
+    let pattendn=/^[A-Za-z]+\w{2,}$/;
+    let ten=document.getElementById('username_text').value;
+    if(!pattendn.test(ten))
+    {
+        document.getElementById('ertendn').innerHTML="Sai tên đăng nhập. Không được chứa ký tự đặc biệt và dấu cách";
+        return false;
+    }
+    else
+    {
+        document.getElementById('ertendn').innerHTML="";
+        return true;
+    }
+    
+  }
+  function ktrtrungmk() {
+    let mk=document.getElementById('txtpwd').value;
+    let mkre=document.getElementById('txtpwdre').value;
+    let result=mk.localeCompare(mkre);
+    if(result)
+    {
+        document.getElementById('erpwdre').innerHTML="Mật khẩu không trùng nhau";
+        return false;
+    }
+    else
+    {
+        document.getElementById('erpwdre').innerHTML="";
+        return true;
+  
+    }
+}  
+sign_up_form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if(ktratendn() && ktrtrungmk()) 
+    {
+      alert('Đăng ký thành công!');
+      // Chuyển hướng đến trang chính của trang web
+      window.location.href = 'Main.html';
+    } 
+    
+  });
