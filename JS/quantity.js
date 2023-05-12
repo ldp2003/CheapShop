8a5c9061a141488a70e7a334935517191b03e430
Pkkoi1 <khoinhokboddy@gmail.com>
2023-05-12 23:42:26 +0700

    Thay đổi thành tiền

--- JS/quantity.js
+++ JS/quantity.js
@@ -1,14 +1,24 @@
 const plusButtons = document.querySelectorAll('#plus');
       const minusButtons = document.querySelectorAll('#minus');
+      
+    //   const totalE = document.querySelector('#total');
+
       plusButtons.forEach(button => {
         button.addEventListener('click', () =>
         {
             const product = button.closest('.cart-product');
-            const countSpan = product.querySelector('.cart_product-item-qty')
+            const countSpan = product.querySelector('.cart_product-item-qty');
             const currentCount = parseInt(countSpan.dataset.count);
+            const unitPriceE = product.querySelector('.cart_product-item-price');
+            const currentPrice = parseFloat(unitPriceE.dataset.price);
+            const totalSpan = product.querySelector('.total');
             const newCount = currentCount + 1;
             countSpan.dataset.count = newCount;
             countSpan.textContent = newCount;
+            const newTotal = newCount * currentPrice;
+            totalSpan.dataset.price = newTotal;
+            totalSpan.textContent = newTotal + ' đ';
+            
         });
         
       });
@@ -18,14 +28,20 @@ const plusButtons = document.querySelectorAll('#plus');
         button.addEventListener('click', () =>
         {
           const product = button.closest('.cart-product');
-            const countSpan = product.querySelector('.cart_product-item-qty')
+            const countSpan = product.querySelector('.cart_product-item-qty');
             const currentCount = parseInt(countSpan.dataset.count);
+            const unitPriceE = product.querySelector('.cart_product-item-price');
+            const currentPrice = parseFloat(unitPriceE.dataset.price);
+            const totalSpan = product.querySelector('.total');
             if(currentCount > 0)
             {
-              const newCount = currentCount - 1;
+            const newCount = currentCount - 1;
             countSpan.dataset.count = newCount;
             countSpan.textContent = newCount;
+            const newTotal = newCount * currentPrice;
+            totalSpan.dataset.price = newTotal;
+            totalSpan.textContent = newTotal + ' đ';
+
             }
-            
         });
       });
